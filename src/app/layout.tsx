0a8325d36064788a2b971c68d462def42d05e96a
Faried Idris <107062781+psyofrelief@users.noreply.github.com>
2026-02-21 20:23:27 +0800

    chore: initialise project structure and directory architecture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,27 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const neueHaasGrotesk = localFont({
+  src: [
+    {
+      path: "../../public/fonts/nHaasGrotDisp/NeueHaasGrotDisp-55Roman.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/nHaasGrotDisp/NeueHaasGrotDisp-65Medium.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/nHaasGrotDisp/NeueHaasGrotDisp-75Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-neue-haas-grotesk-display-pro",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${neueHaasGrotesk.variable} antialiased`}>
         {children}
       </body>
     </html>
